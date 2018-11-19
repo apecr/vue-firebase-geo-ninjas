b65c7ccf21805e6a4b987a95007c7457158fc4b0
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-11-19 16:46:34 +0100

    feat() Include login component
    [FA-139]

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -4,12 +4,9 @@
             <div class="container">
                 <a href="" class="brand-logo left">GeoNinjas!</a>
                 <ul class="right">
-                    <li>
-                        <a href="">
-                            <router-link :to="{name: 'Signup'}">Signup</router-link>
-                        </a>
-                    </li>
-                    <li><a href="">Login</a></li>
+                    <li><router-link :to="{name: 'Signup'}">Signup</router-link></li>
+                    <li><router-link :to="{name: 'Login'}">Login</router-link></li>
+                    <li><a @click="logout">Logout</a></li>
                 </ul>
             </div>
         </nav>
@@ -18,14 +15,32 @@
 
 
 <script>
+import firebase from "firebase";
+
 export default {
   name: "Navbar",
   data() {
     return {};
+  },
+  methods: {
+    async logout() {
+      await firebase.auth().signOut();
+      this.$router.push({ name: "Signup" });
+    }
   }
 };
 </script>
 
 <style>
+.login {
+    max-width: 400px;
+    margin-top: 60px;
+}
+.login h2{
+    font-size: 2.4em;
+}
+.login .field{
+    margin-bottom: 16px;
+}
 </style>
 
